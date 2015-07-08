475eb76d9f3227a760772a75cc7fd4ea88928205
Sebastien Duthil <sduthil@avencall.com>
2015-07-08 14:42:11 -0400

    simplify init-docker.sh

--- init-docker.sh
+++ init-docker.sh
@@ -1,24 +1,11 @@
-#/bin/bash
+#!/bin/sh
 
 CD32=/tmp/cd32
 CD64=/tmp/cd64
 PACKAGES=/tmp/xivo_packages/
 
-if [ $(id -u) -eq 0 ]; then
-
-    if [ ! -d $CD32 ]; then
-        mkdir -p $CD32/images
-    fi
-
-    if [ ! -d $CD64 ]; then
-        mkdir -p $CD64/images
-    fi
-
-    if [ ! -d $PACKAGES ]; then
-        mkdir $PACKAGES
-    fi
-
-    chmod 777 $CD32 $CD32/images $CD64 $CD64/images $PACKAGES
-else
-    echo "Please run as root or with sudo!"
-fi
+install -m 777 -d $CD32
+install -m 777 -d $CD32/images
+install -m 777 -d $CD64
+install -m 777 -d $CD64/images
+install -m 777 -d $PACKAGES
